5d027f560b76b22d12adf7badd2ff5ca51e320bd
eikiu <eikiu.inc@gmail.com>
2016-04-27 22:15:25 -0300

    updated jekyll stuff

--- iniciar.bat
+++ iniciar.bat
@@ -6,4 +6,4 @@ ECHO.
 
 chcp 65001
 REM --safe because GitHub Pages runs it like that (no plugins, ignore symbolic links)
-bundle exec jekyll serve --safe
+bundle exec jekyll serve --safe --watch --incremental
