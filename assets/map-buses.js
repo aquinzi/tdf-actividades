f3ef7fb44ccc1e5fb25a86366da940c8f6529bc0
eikiu <eikiu.inc@gmail.com>
2018-03-02 21:24:42 -0300

    fixes for buses: only show stops corresponding to its line, changed marker descriptions, changed place of download links

--- assets/map-buses.js
+++ assets/map-buses.js
@@ -1,54 +1,74 @@
 function initMap(linea){
-  var map = L.map('map').setView([-53.78903, -67.69588], 14);
+	var map = L.map('map').setView([-53.78903, -67.69588], 14);
 	
-
-  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    {
+	L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
       attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
       maxZoom: 17,
       minZoom: 9
-    }).addTo(map);
+	}).addTo(map);
 
 
-  // load GeoJSON from an external file
-  $.getJSON("/public/colectivos/rio-grande-colectivos-2016-" + linea + ".geojson", function(data){
+	// load GeoJSON from an external file
+	map_file = "";
+	if ( linea == "colectivos-rg-all" ) {
+		map_file = "/public/colectivos/rio-grande-colectivos-2016-todas-lineas.geojson";
+	}
+	else {
+		map_file = "/public/colectivos/rio-grande-colectivos-2016-" + linea + ".geojson";
+	}
+
+	$.getJSON(map_file, function(data){
+		// add GeoJSON layer to the map once the file is loaded
+		L.geoJson(data, {
+			style: function(feature){
+				return { color: feature.properties._storage_options.color};
+			}
+		}).addTo(map);
+	});
+
+	// load GeoJSON from an external file
+	$.getJSON("/public/colectivos/rio-grande-colectivos-paradas.geojson",function(data){
+
+	var busStopIcon_blue = L.icon({
+		iconUrl: '/assets/imgs/bus-stop-blue.png',
+		iconSize: [30,35]
+	}); 
+
+	var busStopIcon_green = L.icon({
+		iconUrl: '/assets/imgs/bus-stop-green.png',
+		iconSize: [30,35]
+	}); 
 
     // add GeoJSON layer to the map once the file is loaded
-    L.geoJson(data, {
-	    style: function(feature){
-	       return { color: feature.properties._storage_options.color};
-	    }
-    }).addTo(map);
-  });
-
-  // load GeoJSON from an external file
-  $.getJSON("/public/colectivos/rio-grande-colectivos-paradas.geojson",function(data){
-
-	 var busStopIcon_blue = L.icon({
-	   iconUrl: '/assets/imgs/bus-stop-blue.png',
-	   iconSize: [30,35]
-	 }); 
-
-	 var busStopIcon_green = L.icon({
-	   iconUrl: '/assets/imgs/bus-stop-green.png',
-	   iconSize: [30,35]
-	 }); 
-
-    // add GeoJSON layer to the map once the file is loaded
-    L.geoJson(data, {
+	L.geoJson(data, {
     	
-			pointToLayer: function(feature,latlng){
+		pointToLayer: function(feature,latlng){
+			
+			//only show bus stops if they are from this line
+			if ( linea != "colectivos-rg-all" && feature.properties.description.indexOf("Línea " + linea.replace("linea-","").toUpperCase()) === -1) {
+				return;
+			}
+			
+			var description_linebreaks = feature.properties.description.replace("\nC: ","<br>").split("\n").join("<br>");
+			var name = feature.properties.name;
 			var marker;
+			var has_garita = "";
 
 			if (feature.properties.name.indexOf('Garita') >= 0) {
 				marker = L.marker(latlng,{icon: busStopIcon_blue});
+				has_garita = "<br>Con garita";
+				name = name.replace(" Garita","")
 			}
 			else{
 				marker = L.marker(latlng,{icon: busStopIcon_green});
 			}
-
-			var description_linebreaks = feature.properties.description.replace("\nC: ","<br>").split("\n").join("<br>");
-			marker.bindPopup(feature.properties.name + '<br>' + description_linebreaks);
+			
+			var must_check = "";
+			if ( !feature.properties.color || feature.properties.color.indexOf("Navy") !== -1 ){
+				must_check = "<br> <strong>La parada debe comprobarse</strong>";
+			}
+			
+			marker.bindPopup(name + has_garita + '<br>' + description_linebreaks + must_check);
 			return marker; 
 			}
     }).addTo(map);
